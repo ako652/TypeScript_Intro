6f92c2f4db77ca8238cebbab698b398ca289f28c
andrea <ngocanhhsgs@gmail.com>
2023-05-24 10:21:00 +0300

    add hw

--- function.ts
+++ function.ts
@@ -2,16 +2,17 @@
 
 // 2. write a function to compare the number user insert to the number 3 then display the suitable message
 
-// 3. write a function that take an array as a parameter then check if user has age greater than 3
-
+// the users array for assignment 3 and 4 below
 const users = [
   { name: "andrea", age: 4 },
   { name: "hanna", age: 9 },
 ];
 
-// 4. write a function takes 2 parameters : an array and userInput to check if the userInput is matched with the user name
+// 3. write a function that take users array as a parameter then check if user has age greater than 3
 
-// At least 2 ways
+// 4. write a function takes 2 parameters : users array and userInput to check if the userInput is matched with the user name
+
+//  2 ways
 
 // searchName(users, "alex"); => alex is not in the users array
 // searchName(users, "andrea"); => andrea is in the users array
